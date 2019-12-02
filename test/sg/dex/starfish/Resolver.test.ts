47e6790ae84c478808447eb68586bdf94b13fca9
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-02 22:55:32 +0800

    mocha test added for Resolver

---
+++ test/sg/dex/starfish/Resolver.test.ts
@@ -0,0 +1,8 @@
+import Resolver from "../../../../src/sg/dex/starfish/Resolver";
+
+describe("Resolver", () => {
+    it("init", ()  => {
+        let resolver = new Resolver();
+        resolver.stop();
+    })
+})
