4514586eb2229eca461353506a7ceadb6a0e9949
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-13 14:26:11 +0800

    gulp build system implemented. both dev with sourcemap and final minified configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,62 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify-es').default;
 
-gulp.task('default', function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest('dist'));
+var paths = {
+    pages: ['src/*.html']
+};
+
+gulp.task('copy-html', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('dev', gulp.series(gulp.parallel('copy-html'), function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/testweb.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('testwebBundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/node'))
+}));
+
+gulp.task('final', gulp.series(gulp.parallel('copy-html'), function () {
+    return browserify({
+        basedir: '.',
+        debug: false,
+        entries: ['src/testweb.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('testwebBundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/node'))
+}));
+
+// Rerun the dev task when a file changes
+gulp.task('watch', function() {
+    gulp.watch('src/*.ts', gulp.parallel('dev'));
+});
+
+gulp.task('build', gulp.parallel('dev', 'final'));
+
+// an alias.
+gulp.task('default', gulp.parallel('build'));
