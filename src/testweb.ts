da3235d5fa915277c97d97f35eecbf7340edc6fe
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-16 15:32:55 +0800

    HTML to TypeScript event binding

--- src/testweb.ts
+++ src/testweb.ts
@@ -1,6 +1,13 @@
 import DirectPurchase from "./sg/dex/starfish/DirectPurchase";
 import MetamaskProviderWeb from "./sg/dex/starfish/Providers/MetamaskProviderWeb";
-document.body.innerHTML = 'Hello Web';
-let mematamskProviderWeb = new MetamaskProviderWeb();
-let directPurchase = new DirectPurchase(mematamskProviderWeb);
-directPurchase.shutdown();
+
+function start(e:Event) {
+    let mematamskProviderWeb = new MetamaskProviderWeb();
+    let directPurchase = new DirectPurchase(mematamskProviderWeb);
+    directPurchase.shutdown();
+}
+
+window.addEventListener('load', function() {
+    var myBtn = window.document.getElementById("myBtn");
+    myBtn.addEventListener("click", (e:Event) => start(e));  
+})
