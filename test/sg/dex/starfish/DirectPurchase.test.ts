dc0701cfa7619e7cd3cf8f1c3ee4d4c552f0c361
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-18 14:36:53 +0800

    DirectProvider added for tests

--- test/sg/dex/starfish/DirectPurchase.test.ts
+++ test/sg/dex/starfish/DirectPurchase.test.ts
@@ -1,5 +1,6 @@
 import DirectPurchase from "../../../../src/sg/dex/starfish/DirectPurchase";
 import MetamaskProvider from "../../../../src/sg/dex/starfish/Providers/MetamaskProvider";
+import DirectProvider from "../../../../src/sg/dex/starfish/Providers/DirectProvider";
 import Config from "../../../../src/Config"
 
 describe("DirectPurchase", () => {
@@ -9,6 +10,20 @@ describe("DirectPurchase", () => {
         const config = new Config();
         await directPurchase.sendTokenAndLog(
             config.values['test_account2'], 
+            config.values['test_account1'],
+            10,
+            "1234",
+            "1234"
+            );
+        await directPurchase.shutdown();
+    })
+
+    it("Test DirectPurchase with DirectHttp provider", async ()  => {
+        const config = new Config();
+        let directProvider = new DirectProvider(config.values['keeper_url']);
+        let directPurchase = new DirectPurchase(directProvider);
+        await directPurchase.sendTokenAndLog(
+            config.values['test_account2'],
             config.values['test_account1'],
             10,
             "1234",
