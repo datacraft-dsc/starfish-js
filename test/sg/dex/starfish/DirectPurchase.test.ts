3e5478d1f127895a15739876b01e69a8f49da3ce
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-23 18:28:24 +0800

    Direct Purchase test updated

--- test/sg/dex/starfish/DirectPurchase.test.ts
+++ test/sg/dex/starfish/DirectPurchase.test.ts
@@ -6,6 +6,7 @@ import Ocean from "../../../../src/Ocean"
 import * as assert from "assert"
 
 describe("DirectPurchase", () => {
+    const tokenNumber = 10;
     xit("init Metamask provider", async ()  => {
         let mematamskProvider = new MetamaskProvider();
         let directPurchase = new DirectPurchase(mematamskProvider);
@@ -26,16 +27,29 @@ describe("DirectPurchase", () => {
         let directPurchase = new DirectPurchase(directProvider);
         const ocean = await Ocean.getInstance(config);
         const squidInstance = await ocean.getSquid();
-        const [account] = await squidInstance.accounts.list();
-        let result = await squidInstance.accounts.requestTokens(account, 10);
+        const accounts = await squidInstance.accounts.list();
+        const receiver = accounts[1];
+        const sender = accounts[0];
+
+        let result = await squidInstance.accounts.requestTokens(sender, tokenNumber);
         assert(result);
-        await directPurchase.sendTokenAndLog(
-            config.values['test_account2'],
-            config.values['test_account1'],
-            10,
+        let balanceSenderBefore = await sender.getOceanBalance();
+        let balanceReceiverBefore = await receiver.getOceanBalance();
+
+        let txReceipt = await directPurchase.sendTokenAndLog(
+            sender.getId(),
+            receiver.getId(),
+            tokenNumber,
             "1234",
             "1234"
             );
+        assert(txReceipt.status);
+
+        let balanceSenderAfter = await sender.getOceanBalance();
+        let balanceReceiverAfter = await receiver.getOceanBalance();
+        assert(balanceSenderAfter + tokenNumber === balanceSenderBefore);
+        assert(balanceReceiverBefore + tokenNumber === balanceReceiverAfter);
+
         await directPurchase.shutdown();
     })
 })
