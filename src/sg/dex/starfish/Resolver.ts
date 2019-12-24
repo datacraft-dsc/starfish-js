409174c012ed6b764bf892a5a04b3103f2a3138c
Ilia Bukalov <ilyabukalov@gmail.com>
2019-12-24 12:57:59 +0800

    didRegistry contract initialization

--- src/sg/dex/starfish/Resolver.ts
+++ src/sg/dex/starfish/Resolver.ts
@@ -1,17 +1,29 @@
-import Provider from "./Providers/MetamaskProvider";
+import Provider from "./Providers/ProviderInterface";
 const Web3 = require('web3');
+import Config from "../../../../src/Config"
+import Ocean from "../../../../src/Ocean"
+
 
 class Resolver {
     private web3;
     private provider;
+    private contract;
+    private accountFrom;
 
-    constructor(provider: Provider) {
+    constructor(provider: Provider, accountFrom: string) {
         this.provider = provider;
         this.web3 = new Web3(this.provider.getProvider());
+        this.accountFrom = accountFrom;
+        const config = new Config();
+        this.contract = Ocean.getInstance(config).then (async (ocean)=> {
+            const squidInstance = await ocean.getSquid();
+            return squidInstance.keeper.didRegistry;
+        });
     }
 
     async shutdown() {
         await this.web3.currentProvider;
+        await this.contract;
         return this.provider.stop();
     }
 
