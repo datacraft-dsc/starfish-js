751643e9e7c267a0021ccb5c51106d31ff1233d9
Ilia Bukalov <ilyabukalov@gmail.com>
2020-01-03 14:35:26 +0800

    Web page for demonstration

--- src/testweb.ts
+++ src/testweb.ts
@@ -1,13 +1,45 @@
 import DirectPurchase from "./sg/dex/starfish/DirectPurchase";
 import MetamaskProviderWeb from "./sg/dex/starfish/Providers/MetamaskProviderWeb";
+let directPurchase;
 
-function start(e:Event) {
+async function start(e:Event) {
     let mematamskProviderWeb = new MetamaskProviderWeb();
-    let directPurchase = new DirectPurchase(mematamskProviderWeb);
-    directPurchase.shutdown();
+    directPurchase = new DirectPurchase(mematamskProviderWeb);
+
+    const publisher = getPublisher();
+    if(!publisher)
+      return;
+
+    const reference = getReference();
+    if(!reference)
+      return;
+
+    directPurchase.subscribe(publisher, reference, document.getElementById("events"));
+}
+
+function getPublisher(){
+    const publisher = document.getElementById("publisher");
+    const value = publisher['value'];
+    if (!value)
+      alert("Empty publisher");
+    return value;
+}
+
+function getReference(){
+    const reference = document.getElementById("reference");
+    const value = reference['value'];
+    if (!value)
+      alert("Empty reference");
+    return value;
+}
+
+async function stop(e:Event) {
+  await directPurchase.shutdown();
 }
 
 window.addEventListener('load', function() {
-    var myBtn = window.document.getElementById("myBtn");
-    myBtn.addEventListener("click", (e:Event) => start(e));  
+    var startBtn = window.document.getElementById("startBtn");
+    startBtn.addEventListener("click", (e:Event) => start(e));
+    var stopBtn = window.document.getElementById("stopBtn");
+    stopBtn.addEventListener("click", (e:Event) => stop(e));
 })
