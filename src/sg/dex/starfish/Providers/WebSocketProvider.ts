d214dcca898f77f086d0b3266e3102f497ca1297
Ilia Bukalov <ilyabukalov@gmail.com>
2020-01-22 14:36:07 +0800

    Web socket provider implemented

---
+++ src/sg/dex/starfish/Providers/WebSocketProvider.ts
@@ -0,0 +1,30 @@
+import Provider from "./ProviderInterface";
+const Web3 = require('web3');
+
+/**
+ * Web3 provider which uses WebSocket
+ * WebSocket provider is the only provider which does suport listening/subscribtion for events. 
+ */
+class WebSocketProvider implements Provider {
+    private provider;
+    getProvider()
+    {
+        return this.provider;
+    }
+
+    stop() {
+    }
+
+    async checkIfProviderEnabled(web3: any) {
+        return true;
+    }
+
+    /**
+     * Constructs the WebSocketProvider
+     * @param endpoint: WS url to connect to Ethereum RPC node.
+     */
+    constructor(endpoint: string) {
+        this.provider = new Web3.providers.WebsocketProvider(endpoint);
+    }
+}
+export default WebSocketProvider;
