88d5e344f7b147d45d1348426ce8bc88f6f961c2
billbsing <bill.barman@dex.sg>
2020-05-28 10:00:35 +0800

    add account creation tests

--- src/Account.ts
+++ src/Account.ts
@@ -5,18 +5,55 @@
 
 */
 
+import fs from 'fs-extra'
+import path from 'path'
+import Web3 from 'web3'
+
+
 export default class Account {
 
     private address: string
     private password: string
-    
-    constructor(address?: string) {
-        this.address = address
+    private keyFilename: string
+    private keyData: Object
+
+    public static createNew(password: string, entropy?: string): Account {
+        let web3 = new Web3()
+        let result = web3.eth.accounts.create(entropy)        
+        let address = result.address
+        let keyData = web3.eth.accounts.encrypt(result.privateKey, password)
+        return new Account(address, password, null, keyData)
     }
+
+    
+    constructor(address?: string, password?: string, keyFilename?: string, keyData?: Object) {
+        this.address = address
+        this.password = password
+        this.keyFilename = keyFilename
+        this.keyData = keyData
+        if (this.keyFilename) {
+            this.loadKeyFile(this.keyFilename)
+        }
+    }
+
+
+    loadKeyFile(filename: string) {
+        if (fs.pathExists(filename)) {
+            this.keyData = import(path.join('../', filename))
+        }
+    }
+
+    
     getAddress(): string {
         return this.address
     }
     getPassword(): string {
         return this.password
+    }
+    getKeyFilename(): string {
+        return this.keyFilename
+    }
+    getKeyData(): Object {
+        return this.keyData
     }
 }
--- test/Account.test.ts
+++ test/Account.test.ts
@@ -1,13 +1,24 @@
 import assert from 'assert'
 
 import Account from '../src/Account'
+import Web3 from 'web3'
 
 
-describe('Account', () => {
-    describe('Account Class', () => {
-        it('should create a new Account object', async () => {
-            const account = new Account()
+describe('Account Class', () => {
+    describe('Creation', () => {
+        it('should create a new empty Account object', async () => {
+            let account = new Account()
             assert(account)
+        })
+        it('should create a new Account object using a password', async () => {
+            let web3 = new Web3()
+            let password = web3.utils.randomHex(16)
+            let account = Account.createNew(password)
+            assert(account)
+            assert.equal(account.getPassword(), password)
+            assert(account.getAddress())
+            assert(account.getKeyData())
+            assert.equal(account.getKeyFilename(), null)
         })
 
     })
