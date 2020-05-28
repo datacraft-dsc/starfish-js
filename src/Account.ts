a39462c11ddabc6e568fa7d34d7c8cd234c9ee14
billbsing <bill.barman@dex.sg>
2020-05-28 11:09:41 +0800

    improve account class

--- src/Account.ts
+++ src/Account.ts
@@ -6,8 +6,9 @@
 */
 
 import fs from 'fs-extra'
-import path from 'path'
 import Web3 from 'web3'
+
+import { toChecksumAddress } from 'web3-utils'
 
 
 export default class Account {
@@ -15,7 +16,7 @@ export default class Account {
     private address: string
     private password: string
     private keyFilename: string
-    private keyData: Object
+    private keyData: any
 
     public static createNew(password: string, entropy?: string): Account {
         let web3 = new Web3()
@@ -25,27 +26,57 @@ export default class Account {
         return new Account(address, password, null, keyData)
     }
 
+    public static async loadKeyFileFromFile( filename: string) {
+        let data = null
+        if (fs.pathExists(filename)) {
+            let rawData = await fs.readFile(filename)
+            data = JSON.parse(rawData)
+        }
+        return data
+    }
+
+    public static async createFromFile(password: string, filename: string): Promise<Account> {
+        let web3 = new Web3()
+        let data = await Account.loadKeyFileFromFile(filename)
+        if (data) {
+            let keyData = await web3.eth.accounts.decrypt(data, password)
+            return new Account(keyData.address, password, filename, keyData)
+        }
+        return null
+    }
     
-    constructor(address?: string, password?: string, keyFilename?: string, keyData?: Object) {
+    constructor(address?: string, password?: string, keyFilename?: string, keyData?: any) {
         this.address = address
         this.password = password
         this.keyFilename = keyFilename
         this.keyData = keyData
-        if (this.keyFilename) {
-            this.loadKeyFile(this.keyFilename)
-        }
-    }
-
-
-    loadKeyFile(filename: string) {
-        if (fs.pathExists(filename)) {
-            this.keyData = import(path.join('../', filename))
-        }
     }
 
     
+    async saveToFile(filename: string): Promise<any> {
+        let data = JSON.stringify(this.keyData)
+        return await fs.writeFile(filename, data)
+    }
+
+    exportKey(): string {
+        return JSON.stringify(this.keyData)
+    }
+
+    importKey(privateKey: string, password: string) {
+        let web3 = new Web3()        
+        this.keyData = web3.eth.accounts.encrypt(privateKey, password)        
+    }
+    isAddressEqual(address: string) {
+        return toChecksumAddress(address) === this.getChecksumAddress()
+    }
+    isPassword(): boolean {
+        return this.password != null
+    }
     getAddress(): string {
         return this.address
+    }
+    getChecksumAddress(): string {
+        return toChecksumAddress(this.address)
     }
     getPassword(): string {
         return this.password
--- test/Account.test.ts
+++ test/Account.test.ts
@@ -1,7 +1,12 @@
 import assert from 'assert'
+import fs from 'fs-extra'
+
 
 import Account from '../src/Account'
 import Web3 from 'web3'
+
+let web3 = new Web3()
+let password = web3.utils.randomHex(16)
 
 
 describe('Account Class', () => {
@@ -11,8 +16,6 @@ describe('Account Class', () => {
             assert(account)
         })
         it('should create a new Account object using a password', async () => {
-            let web3 = new Web3()
-            let password = web3.utils.randomHex(16)
             let account = Account.createNew(password)
             assert(account)
             assert.equal(account.getPassword(), password)
@@ -20,6 +23,45 @@ describe('Account Class', () => {
             assert(account.getKeyData())
             assert.equal(account.getKeyFilename(), null)
         })
-
     })
+    describe('saveToFile', () => {
+        it('should create a new empty Account and save to file', async () => {
+            let filename = '/tmp/testAccount_' + web3.utils.randomHex(16) + '.json'
+            if (fs.pathExists(filename)) {
+                fs.remove(filename)
+            }
+            let account = Account.createNew(password)
+            assert(account)
+            await account.saveToFile(filename)
+            assert(fs.pathExists(filename))
+            fs.remove(filename)
+        })
+    })
+    
+    describe('loadFromFile', () => {
+        it('should load back the account from a key data file', async () => {
+            let filename = '/tmp/testAccount_' + web3.utils.randomHex(16) + '.json'
+            if (fs.pathExists(filename)) {
+                fs.remove(filename)
+            }
+            let account = Account.createNew(password)
+            assert(account)
+            await account.saveToFile(filename)
+            assert(fs.pathExists(filename))
+
+            let savedAccount = await Account.createFromFile(password, filename)
+            assert(savedAccount)
+            assert.equal(savedAccount.getAddress(), account.getAddress())
+            fs.remove(filename)
+        })
+    })
+    
+    describe('common properties', () => {
+        it('should return a checksum address', async () => {
+            let account = Account.createNew(password)
+            assert(account)
+            assert(account.getChecksumAddress())
+        })
+    })
+
 })
