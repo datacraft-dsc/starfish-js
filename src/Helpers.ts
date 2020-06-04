97597a14265540dbf0339e030852b9cb15661819
billbsing <bill.barman@dex.sg>
2020-06-04 10:10:12 +0800

    add did support functions

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -8,6 +8,13 @@ import { fromWei, toWei as web3ToWei, toBN, randomHex } from 'web3-utils'
 
 const NETWORK_DID_METHOD = 'dep'
 
+export interface IDIDFragment {
+    method: string
+    id: string
+    path?: string
+    fragment?: string
+    idHex?: string
+}
 /*
  *
  *
@@ -41,12 +48,12 @@ export function didValidate(did: string): boolean {
     if (!didHeader.test(did)) {
         throw new Error(`DID ${did} must start with "did"`)
     }
-    const didMethod = new RegExp('^did:[a-zA-Z0-9]+:.*')
+    const didMethod = new RegExp('^did:[a-z0-9]+:.*', 'i')
     if (!didMethod.test(did)) {
         throw new Error(`DID ${did} method name must have only a-z, A-Z and 0-9 characters`)
     }
 
-    const didPath = new RegExp('^did:[a-zA-Z0-9]+:[a-fA-F0-9]{64}.*')
+    const didPath = new RegExp('^did:[a-z0-9]+:[a-f0-9]{64}/?.*', 'i')
     if (!didPath.test(did)) {
         throw new Error(`DID ${did} path should only have 64 HEX characters`)
     }
@@ -66,9 +73,35 @@ export function didGenerateRandom(): string {
     return idToDID(randomHex(32))
 }
 
+export function didParse(did): IDIDFragment {
+    didValidate(did)
+    const didRegexp = new RegExp('^did:([a-z0-9]+):([a-f0-9]{64})(.*)', 'i')
+    const matchDID = didRegexp.exec(did)
+    const result = {
+        method: matchDID[1],
+        id: matchDID[2],
+    }
+    result['idHex'] = `0x${result['id']}`
+    if (matchDID[3]) {
+        result['path'] = matchDID[3]
+        const fragmentRegexp = new RegExp('(/[a-f0-9]+)(#.*)', 'i')
+        const fragmentMatch = fragmentRegexp.exec(result['path'])
+        if (fragmentMatch) {
+            result['path'] = fragmentMatch[1]
+            result['fragment'] = fragmentMatch[2]
+        }
+    }
+    return result
+}
+
 export function idToDID(id: string): string {
     const cleanId = removeLeadingHexZero(id)
     return `did:${NETWORK_DID_METHOD}:${cleanId}`
+}
+
+export function didToId(did: string): string {
+    const result = didParse(did)
+    return result['idHex']
 }
 
 export function removeLeadingHexZero(text: string): string {
--- test/Helpers.test.ts
+++ test/Helpers.test.ts
@@ -5,8 +5,13 @@
 */
 // import assert from 'assert'
 import {assert, expect} from 'chai'
-
-import { didGenerateRandom, didValidate, removeLeadingHexZero } from '../src/Helpers'
+import {randomHex} from 'web3-utils'
+import {
+    didGenerateRandom,
+    didValidate,
+    removeLeadingHexZero,
+    didParse
+} from '../src/Helpers'
 
 
 describe('DID Helper', () => {
@@ -29,7 +34,6 @@ describe('DID Helper', () => {
             didValidate('did:method:1234')
         }).to.throw(/path should only have 64 HEX characters/)
     })
-
     it('should remove leading 0x char from a hex string', async () => {
         assert.equal(removeLeadingHexZero('0xabcdef1234'), 'abcdef1234')
         assert.equal(removeLeadingHexZero('0Xabcdef1234'), 'abcdef1234')
@@ -37,5 +41,39 @@ describe('DID Helper', () => {
         assert.equal(removeLeadingHexZero('01234'), '01234')
         assert.equal(removeLeadingHexZero('x01234'), 'x01234')
     })
-
+    it('should parse did to get minimum parts', async () => {
+        const testId = removeLeadingHexZero(randomHex(32))
+        const testDID = `did:dep:${testId}`
+        const result = didParse(testDID)
+        assert(result)
+        assert.equal(result['method'], 'dep')
+        assert.equal(result['id'], testId)
+        assert.equal(result['idHex'], `0x${testId}`)
+        assert.isUndefined(result['path'])
+        assert.isUndefined(result['fragment'])
+    })
+    it('should parse did to get path', async () => {
+        const testId = removeLeadingHexZero(randomHex(32))
+        const testAssetId = removeLeadingHexZero(randomHex(32))
+        const testDID = `did:dep:${testId}/${testAssetId}`
+        const result = didParse(testDID)
+        assert(result)
+        assert.equal(result['method'], 'dep')
+        assert.equal(result['id'], testId)
+        assert.equal(result['idHex'], `0x${testId}`)
+        assert.equal(result['path'], `/${testAssetId}`)
+        assert.isUndefined(result['fragment'])
+    })
+    it('should parse did to get path and fragment', async () => {
+        const testId = removeLeadingHexZero(randomHex(32))
+        const testAssetId = removeLeadingHexZero(randomHex(32))
+        const testDID = `did:dep:${testId}/${testAssetId}#fragment`
+        const result = didParse(testDID)
+        assert(result)
+        assert.equal(result['method'], 'dep')
+        assert.equal(result['id'], testId)
+        assert.equal(result['idHex'], `0x${testId}`)
+        assert.equal(result['path'], `/${testAssetId}`)
+        assert.equal(result['fragment'], '#fragment')
+    })
 })
