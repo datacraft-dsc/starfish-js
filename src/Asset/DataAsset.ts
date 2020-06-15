bca372191c82432f6705c2ce0ff1989dc460ad04
billbsing <bill.barman@dex.sg>
2020-06-15 16:17:02 +0800

    asset data create from file, save to file

--- src/Asset/DataAsset.ts
+++ src/Asset/DataAsset.ts
@@ -6,6 +6,9 @@
  *
  */
 
+import fs from 'fs-extra'
+import mime from 'mime-types'
+
 import { IMetadata, IMetadataData } from '../Interfaces/Metadata'
 import { AssetBase } from './AssetBase'
 import { calcAssetDataHash } from '../Crypto'
@@ -19,9 +22,30 @@ export class DataAsset extends AssetBase {
         return asset
     }
 
+    public static async createFromFile(
+        name: string,
+        filename: string,
+        metadata?: string | IMetadataData,
+        did?: string
+    ): Promise<DataAsset> {
+        const storeMetadata = AssetBase.generateMetadata(name, 'dataset', metadata)
+        const data = await fs.readFile(filename)
+        storeMetadata['contentType'] = 'application/octet-stream'
+        const mimeType = mime.lookup(filename)
+        if (mimeType) {
+            storeMetadata['contentType'] = mimeType
+        }
+        const asset = new DataAsset(storeMetadata, did, data)
+        return asset
+    }
+
     constructor(metadata: string | IMetadataData, did?: string, data?: Buffer) {
         super(<IMetadata>metadata, did)
         this.data = data
         this.metadata['contentHash'] = calcAssetDataHash(data)
+    }
+
+    public async saveToFile(filename: string): Promise<void> {
+        fs.writeFile(filename, this.data)
     }
 }
--- test/unit/Asset/DataAsset.test.ts
+++ test/unit/Asset/DataAsset.test.ts
@@ -5,13 +5,15 @@
 */
 
 import { assert } from 'chai'
+import fs from 'fs-extra'
+
 
 import { randomHex, hexToBytes } from 'web3-utils'
 
 import { DataAsset } from 'starfish/Asset/DataAsset'
 
 describe('DataAsset Class', () => {
-    describe('DataAsset constructor', () => {
+    describe('create', () => {
         it('should create a new DataAsset object', async () => {
             const data = Buffer.from(hexToBytes(randomHex(1024)))
             const name = 'newDataAsset'
@@ -21,6 +23,42 @@ describe('DataAsset Class', () => {
             assert(asset.metadataText)
             assert.equal(asset.metadata['name'], name)
             assert.equal(asset.metadata['type'], 'dataset')
+            assert(asset.data.equals(data))
+        })
+    })
+    describe('createFromFile', () => {
+        const filename = '/tmp/testAssetDataFile.dat'
+        let data
+        before( () => {
+            data = Buffer.from(hexToBytes(randomHex(1024)))
+            fs.writeFile(filename, data)
+        })
+        it('should create a new DataAsset object from a file', async () => {
+            const name = 'newDataAsset'
+            const asset = await DataAsset.createFromFile(name, filename)
+            assert(asset)
+            assert(asset.metadata)
+            assert(asset.metadataText)
+            assert.equal(asset.metadata['name'], name)
+            assert.equal(asset.metadata['type'], 'dataset')
+            assert(asset.data.equals(data))
+            assert.equal(asset.metadata['contentType'], 'application/octet-stream')
+        })
+        after( () => {
+            fs.remove(filename)
+        })
+    })
+    describe('saveToFile', () => {
+        const filename = '/tmp/testAssetDataFile.dat'
+        it('should save the data to a file', async () => {
+            const data = Buffer.from(hexToBytes(randomHex(1024)))
+            const name = 'newDataAsset'
+            const asset = DataAsset.create(name, data)
+            await asset.saveToFile(filename)
+            assert(fs.exists(filename))
+        })
+        after( () => {
+            fs.remove(filename)
         })
     })
 })
